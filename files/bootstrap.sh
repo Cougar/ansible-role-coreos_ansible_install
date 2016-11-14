43de19e1ad7ae636aae87d538444f557462275ea
Cougar <cougar@random.ee>
2016-11-14 16:48:55 +0200

    Initial commit
    This role is based on information from https://www.tazj.in/en/1410951452

---
+++ files/bootstrap.sh
@@ -0,0 +1,34 @@
+#/bin/bash
+
+set -e
+
+cd
+
+if [[ -e $HOME/.toolboxrc ]]; then
+  exit 0
+fi
+
+cat > $HOME/.toolboxrc <<-EOF
+	TOOLBOX_DOCKER_IMAGE=v6net/coreos-ansible-toolbox
+	TOOLBOX_USER=root
+EOF
+
+sudo mkdir --parents '/opt/bin'
+
+sudo tee '/opt/bin/python' > /dev/null <<-EOL
+	#!/bin/bash
+	toolbox --quiet --bind=/home:/home --bind=/var/run/docker.sock:/var/run/docker.sock python "\$@"
+EOL
+
+sudo chmod +x '/opt/bin/python'
+
+sudo tee '/opt/bin/pip' > /dev/null <<-EOL
+	#!/bin/bash
+	toolbox --quiet --bind=/home:/home --bind=/var/run/docker.sock:/var/run/docker.sock pip "\$@"
+EOL
+
+sudo chmod +x '/opt/bin/pip'
+
+toolbox mkdir -p /opt/bin
+
+toolbox ln -sf /usr/bin/python /opt/bin
